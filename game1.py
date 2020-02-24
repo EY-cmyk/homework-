9d9987b2c1bfaee6f3d04f975571074c7723a793
EY-cmyk <61290430+EY-cmyk@users.noreply.github.com>
2020-02-24 19:09:41 +0200

    Add files via upload

---
+++ game1.py
@@ -0,0 +1,30 @@
+import random
+guessT = 0
+print("Желаешь заработать??Как тебя зовут?")
+name = input("Введите имя: ")
+num = random.randint(0,20)
+ 
+print("Привет {} ,тогда давай начнём) Угадай число" .format(name))
+while guessT < 6:
+    print("Как ты думаешь,какое?")
+    guessnum = input()
+    guessnum = int(guessnum)
+    guessT = guessT+1
+
+    if guessnum < num:
+        print("Загаданое число больше чем твоё( ")
+    elif guessnum > num:
+        print("Загаданое число меньше чем твоё( ")
+    elif guessnum == num:
+        break
+
+if guessnum == num:
+    guessT = str(guessT)
+    print("Ты угадал!Поздравляю тебя,ты выиграл мешок картошки))")
+elif guessnum != num:
+    num = str(num)
+    print("Ты не угадал, та и попытки закончились ,с тебя милион долларов(( ") 
+    print("Загаданое число было:",num)
+    
+
+
